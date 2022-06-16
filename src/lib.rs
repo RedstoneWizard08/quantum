d05d2c1c84df39d0b10f02ea0e9aeadc06c96b32
Jacob Sapoznikow <500074@bsd48.org>
2022-06-16 20:59:20 +0000

    Fixes and async!

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub use libic::*;
 
-pub async fn get_quilt_versions() -> MavenVersionResult {
+pub async fn get_quilt_versions() -> Vec<String> {
     let resp = util::get(
         "https://maven.quiltmc.org/repository/release/org/quiltmc/quilt-loader/maven-metadata.xml"
             .to_string(),
@@ -8,5 +8,5 @@ pub async fn get_quilt_versions() -> MavenVersionResult {
     let raw = xmltojson::to_json(&resp).expect("Something went wrong.");
     let serialized = serde_json::to_string(&raw).unwrap();
     let des: RawMavenVersionResult = serde_json::from_str(&serialized).unwrap();
-    return des.metadata;
+    return des.metadata.versioning.versions.version;
 }
